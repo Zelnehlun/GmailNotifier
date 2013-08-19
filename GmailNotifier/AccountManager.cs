8396777eed2b27f511237046492e742fa81f9443
Zelnehlun <zelnehlun@gmail.com>
2013-08-19 11:18:27 +0200

    FileUtil and AccountManager

--- GmailNotifier/AccountManager.cs
+++ GmailNotifier/AccountManager.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace GmailNotifier
 {
     public class AccountManager
     {
+        private ICollection<Account> accounts = new List<Account>();
+
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+            SaveAccounts();
+        }
+
+        public void LoadAccounts()
+        {
+            string file = getFilePath();
+            string json = FileUtil.ReadAllText(file);
+
+            if (json != null)
+            {
+                accounts = JsonConvert.DeserializeObject<ICollection<Account>>(json);
+            }
+        }
+
+        public void SaveAccounts()
+        {
+            string file = getFilePath();
+            string json = JsonConvert.SerializeObject(accounts);
+
+            FileUtil.WriteAllText(file, json);
+        }
+
+        private string getFilePath()
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+   
+            return path + "/accounts.json";
+        }
     }
 }
---
+++ GmailNotifier/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GmailNotifier
+{
+    public static class FileUtil
+    {
+        public static string ReadAllText(string file)
+        {
+            if (File.Exists(file))
+            {
+                return File.ReadAllText(file, Encoding.UTF8);
+            }
+
+            return null;
+        }
+
+        public static void WriteAllText(string file, string text)
+        {
+            File.WriteAllText(file, text, Encoding.UTF8);
+        }
+    }
+}
