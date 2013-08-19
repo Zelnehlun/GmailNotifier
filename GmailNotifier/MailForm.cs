8ee4cd6a95b06ff20ddd43e0ed57f9a4762f0584
Zelnehlun <zelnehlun@gmail.com>
2013-08-19 09:09:49 +0200

    Added icons

--- GmailNotifier/MailForm.cs
+++ GmailNotifier/MailForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,48 @@ using System.Windows.Forms;
 
 namespace GmailNotifier
 {
-    public sealed class MailForm : Form
+    public class MailForm : Form
     {
+        private ContextMenu trayMenu;
+        private NotifyIcon trayIcon;
+
+        public MailForm()
+        {
+            trayMenu = new ContextMenu();
+
+            trayMenu.MenuItems.Add("View Inbox");
+            trayMenu.MenuItems.Add("Check Mail Now");
+            trayMenu.MenuItems.Add("Tell me Again...");
+            trayMenu.MenuItems.Add("Options");
+            trayMenu.MenuItems.Add("About...");
+            trayMenu.MenuItems.Add("Exit", onExit);
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "No unread mail";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+
+        private void onExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
     }
 }
